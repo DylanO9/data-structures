747f6ad101ce3aa77b03aadacf1a3b1fc8cd14e5
DylanO9 <dylan.olivares11@gmail.com>
2025-11-10 07:49:53 +0000

    feat: add append function to become an operator for our vector data structure

--- arrays/main.c
+++ arrays/main.c
@@ -3,7 +3,10 @@
 */
 
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+
+#define DEFAULT_CAPACITY    4
 
 typedef struct {
     void *data; // data is a pointer to the beginning of the vector
@@ -13,10 +16,13 @@ typedef struct {
 } Vector;
 
 Vector *init_vector(size_t elem_size);
+void append(Vector *v, void *object);
 
 // Driver function
 int main() {
     Vector *new_vector = init_vector(sizeof(int)); 
+    int x = 5;
+    new_vector(v, &x);
     return 0;
 }
 
@@ -24,9 +30,23 @@ Vector *init_vector(size_t elem_size) {
     Vector *new_vector = (Vector *)malloc(sizeof(Vector));
     new_vector->elem_size = elem_size;
     new_vector->size = 0;
-    new_vector->capacity = 0;
+    new_vector->capacity = DEFAULT_CAPACITY;
+    new_vector->data = malloc(new_vector->elem_size * new_vector->capacity);
     return new_vector;
 }
 
+void append(Vector *v, void *object) {
+    if (v->size + 1 > v->capacity) {
+        size_t new_size = v->capacity * 2;
+        v->data = realloc(v->data, new_size);
+        if (v->data == NULL) {
+            fprintf(stderr, "Bad realloc\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    void *next_element = v->data + (v->size * v->elem_size); 
+    memcpy(next_element, object, v->elem_size);
+    v->size++;
+}
 
 
