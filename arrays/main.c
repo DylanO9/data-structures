d8892739b54df945a5a2f9ef90b07e1cf47cf72a
DylanO9 <dylan.olivares11@gmail.com>
2025-11-11 07:36:58 +0000

    fix: add malloc checks, function of return type int should return int, remove extra parameter in search()

--- arrays/main.c
+++ arrays/main.c
@@ -18,7 +18,7 @@ typedef struct {
 Vector *init_vector(size_t elem_size);
 void append(Vector *v, void *object);
 void *get(Vector *v, unsigned int index);
-void *search(Vector *v, void *object, size_t elem_size);
+void *search(Vector *v, void *object);
 int pop(Vector *v);
 
 // Driver function
@@ -40,7 +40,7 @@ int main() {
         return 0;
     }
     printf("My prev value: %d\nv[0]: %d\n", x, *my_x);
-    void *temp = search(v, &y, sizeof(int));
+    void *temp = search(v, &y);
     printf("Did I find my y element? %d\nWhat was my element? %d\n", 1 ? temp != NULL : 0, *(int *)temp);
     pop(v);
     int *t_6 = get(v, 6);
@@ -50,10 +50,18 @@ int main() {
 
 Vector *init_vector(size_t elem_size) {
     Vector *new_vector = (Vector *)malloc(sizeof(Vector));
+    if (new_vector == NULL) {
+        fprintf(stderr, "Could not create a new vector\n");
+        return NULL;
+    }
     new_vector->elem_size = elem_size;
     new_vector->size = 0;
     new_vector->capacity = DEFAULT_CAPACITY;
     new_vector->data = malloc(new_vector->elem_size * new_vector->capacity);
+    if (new_vector->data == NULL) {
+        fprintf(stderr, "Could not create a new vector\n");
+        return NULL;
+    }
     return new_vector;
 }
 
@@ -68,29 +76,25 @@ void append(Vector *v, void *object) {
         v->data = new_data;
         v->capacity = new_capacity;
     }
-    void *next_element = v->data + (v->size * v->elem_size); 
+    void *next_element = (char *)v->data + (v->size * v->elem_size); 
     memcpy(next_element, object, v->elem_size);
     v->size++;
 }
 
 void *get(Vector *v, unsigned int index) {
-    if (index < 0 || index > v->size - 1) {
+    if (index >= v->size) {
         fprintf(stderr, "Trying to access an out of range index\n");
         return NULL;
     } 
-    return v->data + (v->elem_size * index);
+    return (char *)v->data + (v->elem_size * index);
 }
 
-void *search(Vector *v, void *object, size_t elem_size) {
-    if (elem_size != v->elem_size) {
-        fprintf(stderr, "Trying to search for an object that is not the same as the objects stored in the vector\n");
-        return NULL;
-    }
-    
+void *search(Vector *v, void *object) {
     for (int i = 0; i < v->size; i++) {
         // Check if this data for the next elem_size is == the object of elem_size
-        void *iterator_address = v->data + (i * v->elem_size);    
-        if (memcmp(iterator_address, object, elem_size) == 0)
+        char *base = (char *)v->data;
+        void *iterator_address = base + (i * v->elem_size);    
+        if (memcmp(iterator_address, object, v->elem_size) == 0)
             return iterator_address;
     }
     return NULL;
@@ -98,7 +102,9 @@ void *search(Vector *v, void *object, size_t elem_size) {
 
 // We don't have to decrease the capacity of our array, so we can just clear the bits
 int pop(Vector *v) {
+    if (v->size == 0) return 0;
     void *last_address = v->data + (v->elem_size * (v->size - 1));
     memset(last_address, 0, v->elem_size);
     v->size--;
+    return 1;
 }
