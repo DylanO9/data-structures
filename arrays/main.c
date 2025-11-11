906d4ecb023f8dcb9a2a9c65e4e2091e5ede0001
DylanO9 <dylan.olivares11@gmail.com>
2025-11-11 06:42:54 +0000

    feat: add function search to look for an object with an elem_size within our vector v

--- arrays/main.c
+++ arrays/main.c
@@ -18,6 +18,7 @@ typedef struct {
 Vector *init_vector(size_t elem_size);
 void append(Vector *v, void *object);
 void *get(Vector *v, unsigned int index);
+void *search(Vector *v, void *object, size_t elem_size);
 
 // Driver function
 int main() {
@@ -30,12 +31,16 @@ int main() {
     append(v, &x);
     append(v, &x);
     append(v, &x);
+    int y = 10;
+    append(v, &y);
     int *my_x = get(v, 6);
     if (my_x == NULL) {
         printf("My access was bad!\n");
         return 0;
     }
     printf("My prev value: %d\nv[0]: %d\n", x, *my_x);
+    void *temp = search(v, &y, sizeof(int));
+    printf("Did I find my y element? %d\nWhat was my element? %d\n", 1 ? temp != NULL : 0, *(int *)temp);
     return 0;
 }
 
@@ -51,7 +56,7 @@ Vector *init_vector(size_t elem_size) {
 void append(Vector *v, void *object) {
     if (v->size + 1 > v->capacity) {
         size_t new_capacity = v->capacity * 2;
-        void *new_data = realloc(v->data, (new_capacity * v->elem_size));
+        void *new_data = realloc(v->data, (new_capacity * v->elem_size)); // Could use v->data, but we do not want to lose v->data if our new data allocated is NULL
         if (new_data == NULL) {
             fprintf(stderr, "Bad realloc\n");
             exit(EXIT_FAILURE);
@@ -70,4 +75,19 @@ void *get(Vector *v, unsigned int index) {
         return NULL;
     } 
     return v->data + (v->elem_size * index);
+}
+
+void *search(Vector *v, void *object, size_t elem_size) {
+    if (elem_size != v->elem_size) {
+        fprintf(stderr, "Trying to search for an object that is not the same as the objects stored in the vector\n");
+        return NULL;
+    }
+    
+    for (int i = 0; i < v->size; i++) {
+        // Check if this data for the next elem_size is == the object of elem_size
+        void *iterator_address = v->data + (i * v->elem_size);    
+        if (memcmp(iterator_address, object, elem_size) == 0)
+            return iterator_address;
+    }
+    return NULL;
 }
